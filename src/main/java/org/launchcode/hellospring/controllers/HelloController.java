7358c5cdbc83e128e130e60a042742e8227ab99a
amesp73 <amitrpatel502@gmail.com>
2023-09-27 19:59:59 -0500

    exercises for Thymeleaf

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,88 +1,58 @@
 package org.launchcode.hellospring.controllers;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-//@ResponseBody //if it is here, you do not need it for each method below, they adopt it from above
-//@RequestMapping("hello")
 public class HelloController {
+    //handle request at path http://localhost:8080/hello
 
-    // Handles request at path /hello
 //    @GetMapping("hello")
 //    @ResponseBody
-//    public String hello() {
+//    public String hello(){
 //        return "Hello, Spring!";
 //    }
 
-    // Handles request at path /goodbye
-    // with request mapping up above, lives at /hello/goodbye
+    //handle request at path //http://localhost:8080/goodbye
     @GetMapping("goodbye")
     @ResponseBody
-    public String goodbye() {
+    public String goodbye(){
         return "Goodbye, Spring!";
     }
 
-    // Handles request of the form /hello?name=LaunchCode
-    // "" lives at /hello/hello
+    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model){
+        String theGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
     }
 
-    // Handles requests of the form /hello/LaunchCode
+    //handle requests of the form http://localhost:8080/hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
-    // Handles request at path /form
     @GetMapping("form")
-    @ResponseBody
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" + // submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 
-//    @RequestMapping(value="hello", method = RequestMethod.POST)
-//    @ResponseBody
-//    public String helloPost(@RequestParam String name, @RequestParam String language) {
-//        if (name == null) {
-//            name = "World";
-//        }
-//
-//        return createMessage(name, language);
-//
-//    }
-//
-//    public static String createMessage(String n, String l) {
-//        String greeting = "";
-//
-//        if (l.equals("english")) {
-//            greeting = "Hello";
-//        }
-//        else if (l.equals("french")) {
-//            greeting = "Bonjour";
-//        }
-//        else if (l.equals("italian")) {
-//            greeting = "Bonjourno";
-//        }
-//        else if (l.equals("spanish")) {
-//            greeting = "Hola";
-//        }
-//        else if (l.equals("german")) {
-//            greeting = "Hallo";
-//        }
-//
-//        return greeting + " " + n;
-//    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("Launch");
+        names.add("Code");
+        names.add("Java");
+        names.add("Javascript");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
 }
